fc6a8d213199196d8478748c9baa9b4cbc404185
Vovchikus <rampart@yandex.ru>
2019-01-16 11:44:41 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+function getPath(url) {
+    var pathRegex = /.+?\:\/\/.+?(\/.+?)(?:#|\?|$)/;
+    var result = url.match(pathRegex);
+    return result && result.length > 1 ? result[1] : ''; 
+}
+ 
+function getQueryString(url) {
+    var arrSplit = url.split('?');
+    return arrSplit.length > 1 ? url.substring(url.indexOf('?')+1) : ''; 
+}
+ 
+function getAuthHeader(httpMethod, requestUrl, requestBody) {
+    var CLIENT_KEY = 'key';
+    var SECRET_KEY = 'secret';
+    var AUTH_TYPE = 'HMAC-SHA256';
+ 
+    var requestPath = getPath(requestUrl);
+    
+    var queryString = getQueryString(requestUrl);
+    
+    if (httpMethod == 'GET' || !requestBody) {
+        requestBody = ''; 
+    }
+         
+    var hashedPayload = CryptoJS.enc.Hex.stringify(CryptoJS.SHA256(requestBody));
+    var timestamp = new Date().toISOString().split('.')[0];
+    var requestData = [httpMethod, requestPath, queryString, timestamp, hashedPayload].join(" ");
+    var hashedRequestData = CryptoJS.enc.Hex.stringify(CryptoJS.SHA256(requestData));
+    var hmacDigest = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA256(hashedRequestData, SECRET_KEY));
+    var authHeader = AUTH_TYPE + ', ' + timestamp + ", " + CLIENT_KEY + ', ' + hmacDigest;
+        
+    return authHeader;
+}
+ 
+postman.setEnvironmentVariable('hmacAuthHeader', getAuthHeader(request['method'], request['url'], request['data']));
+
+
